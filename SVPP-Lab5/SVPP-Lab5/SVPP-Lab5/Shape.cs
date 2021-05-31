1acd404661ebb984d5bc8cad63fc15a98c56b7a7
kalilask4 <kalilask4@gmail.com>
2021-06-01 00:35:01 +0300

    add class Shape and new dialog window

---
+++ SVPP-Lab5/SVPP-Lab5/SVPP-Lab5/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SVPP_Lab5
+
+{
+    class Shape
+    {
+
+
+        public int Widght { get; set; }
+        public Brush Background { get; set; }
+        public Brush Foreground { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Shape()
+        {
+        }
+
+        public Shape(int widght, Brush background, Brush foreground, double x, double y)
+        {
+            Widght = widght;
+            Background = background;
+            Foreground = foreground;
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"Widght = {Widght}, Background = {Background}, Foreground = {Foreground}, ({X};{Y})";
+        }
+    }
+}
