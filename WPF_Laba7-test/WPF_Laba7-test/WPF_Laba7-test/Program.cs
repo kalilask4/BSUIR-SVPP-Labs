09dc3f0fbbd7e80d5842e09ab2039d6aa82feeeb
kalilask4 <kalilask4@gmail.com>
2021-06-04 02:38:06 +0300

    Init test project lab 7. Added Models

---
+++ WPF_Laba7-test/WPF_Laba7-test/WPF_Laba7-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPF_Laba7_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
