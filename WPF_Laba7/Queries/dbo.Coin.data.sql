dd5604f73391c3391b7936d7432104356087983e
kalilask4 <kalilask4@gmail.com>
2021-06-08 23:36:48 +0300

    add Show + query to db

---
+++ WPF_Laba7/Queries/dbo.Coin.data.sql
@@ -0,0 +1,8 @@
+﻿
+
+select * from Coin
+
+INSERT INTO Coin (Title, Amount, CrtPrice, CrtCost) values('bnb', '20', '403', '860.00' )
+INSERT INTO Coin (Title, Amount, CrtPrice, CrtCost) values('xrp', '750', '0.985', '738.75' )
+INSERT INTO Coin (Title, Amount, CrtPrice, CrtCost) values('ltc', '20', '403', '860.00' )
+
--- WPF_Laba7/WPF_Laba7/WPF_Laba7/MainWindow.xaml.cs
+++ WPF_Laba7/WPF_Laba7/WPF_Laba7/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using WPF_Laba7.Entities;
 
 namespace WPF_Laba7
 {
@@ -21,10 +22,31 @@ namespace WPF_Laba7
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Coin coin;
+        ObservableCollection<Coin> collection = new ObservableCollection<Coin>();
+
         public MainWindow()
         {
             InitializeComponent();
+            coin = new Coin();
+            gridCoin.DataContext = coin;
+            listBox.DataContext = collection;
 
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            collection.Clear();
+            foreach (var p in Coin.getAllCoin())
+            {
+                collection.Add(p);
+            }
         }
     }
 }
