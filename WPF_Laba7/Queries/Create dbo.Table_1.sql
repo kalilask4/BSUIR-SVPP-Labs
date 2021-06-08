6ef1da3cb006e8c851cab62eb84aa9da6451f817
kalilask4 <kalilask4@gmail.com>
2021-06-08 23:29:37 +0300

    create and connect db

---
+++ WPF_Laba7/Queries/Create dbo.Table_1.sql
@@ -0,0 +1,20 @@
+﻿USE [lab7db]
+GO
+
+/****** Object: Table [dbo].[Table] Script Date: 08.06.2021 23:19:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Coin] (
+    [CoinId]   INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (50) NOT NULL,
+    [Amount]   FLOAT (53)    NOT NULL,
+    [CrtPrice] FLOAT (53)    NOT NULL,
+    [CrtCost]  FLOAT (53)    NOT NULL
+);
+
+
+
--- WPF_Laba7/WPF_Laba7/WPF_Laba7/Coin.cs
+++ WPF_Laba7/WPF_Laba7/WPF_Laba7/Coin.cs
@@ -16,10 +16,10 @@ namespace WPF_Laba7.Entities
         public int CoinId { get; set; }
         public string Title { get; set; }
         public double Amount { get; set; }
-        public double AvgPrice { get; set; } //the average price should be calculated based on transactions at different times
-        public double CurPrice { get; set; } //the current price should ideally be loaded from binance
-        public double AvgCost { get; set; } //Amount*AvgPrice
-        public double CurCost { get; set; } //Amount*CurPrice
+        //public double AvgPrice { get; set; } //the average price should be calculated based on transactions at different times
+        public double CrtPrice { get; set; } //the current price should ideally be loaded from binance
+        //public double AvgCost { get; set; } //Amount*AvgPrice
+        public double CrtCost { get; set; } //Amount*CurPrice
 
 
         public static void newConnection()
@@ -29,40 +29,48 @@ namespace WPF_Laba7.Entities
             connection.Open();
         }
 
-        public override string ToString()
-        {
-            return $"{CoinId}. {Title}, Amount - {Amount}, CurPrice ${CurPrice}, Cost ${CurCost})";
 
+        public static IEnumerable<Coin> getAllCoin()
+        {
+            newConnection();
+            var commandString = "SELECT * FROM Coin";
+            SqlCommand sqlCommand = new SqlCommand(commandString, connection);
+            var reader = sqlCommand.ExecuteReader();    //запрос возвращает данные
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32(0);
+                    var title = reader.GetString(1);
+                    var amount = reader.GetDouble(2);
+                    var crtPrice = reader.GetDouble(3);
+                    var crtCost = reader.GetDouble(4);
+
+                    var coin = new Coin
+                    {
+                        CoinId = id,
+                        Title = title,
+                        Amount = amount,
+                        CrtPrice = crtPrice,
+                        CrtCost = crtCost
+                    };
+
+                    yield return coin;
+                }
+            }
+            connection.Close();
         }
 
 
 
 
 
-        //public Coin()
-        //{
-        //    var connString = ConfigurationManager
-        //        .ConnectionStrings["DemoConnection"]
-        //        .ConnectionString;
 
-        //    connection = new SqlConnection(connString);
-        //}
-        //static Coin()
-        //{
-        //    var connString = ConfigurationManager
-        //        .ConnectionStrings["DemoConnection"]
-        //        .ConnectionString;
+        public override string ToString()
+        {
+            return $"{CoinId}. {Title}, amount - {Amount}, current price ${CrtPrice}, cost ${CrtCost})";
 
-        //    connection = new SqlConnection(connString);
-        //}
-
-        //public static IEnumerable<Coin> GetAllCoins()
-        //{
-        //    var commandString = "Select * FROM Coin";
-        //}
-
-
-
+        }
 
 
     }
