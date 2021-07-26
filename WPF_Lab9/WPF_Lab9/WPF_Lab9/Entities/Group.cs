bd8fc9d43b378bf42572e94ebf1771b3a0b32df6
kalilask4 <kalilask4@gmail.com>
2021-07-26 18:57:36 +0300

    add entities

---
+++ WPF_Lab9/WPF_Lab9/WPF_Lab9/Entities/Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Lab9.Entities
+{
+    class Group
+    {
+    }
+}
