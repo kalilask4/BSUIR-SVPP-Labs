ff3e11de9d6989895b0fc5c567fb47562bcc3ba3
kalilask4 <kalilask4@gmail.com>
2021-07-26 18:59:50 +0300

    add context

---
+++ WPF_Lab9/WPF_Lab9/WPF_Lab9/EFContext/CoursesContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Lab9.Entities;
+
+namespace WPF_Lab9.EFContext
+{
+    class CoursesContext:DbContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Group> groups { get; set; }
+
+        public CoursesContext()
+        { }
+
+
+    }
+}
