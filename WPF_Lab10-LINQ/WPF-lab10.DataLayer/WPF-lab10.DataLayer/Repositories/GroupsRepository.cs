1bca32c003288dda91bd4f5368d1b802b61b7fcf
kalilask4 <kalilask4@gmail.com>
2021-07-30 21:06:03 +0300

    GroupsRepository  implemented

--- WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/Repositories/GroupsRepository.cs
+++ WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/Repositories/GroupsRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_lab10.DataLayer.EFContext;
 using WPF_lab10.DataLayer.Entities;
 using WPF_lab10.DataLayer.Interfaces;
 
@@ -10,39 +12,44 @@ namespace WPF_lab10.DataLayer.Repositories
 {
     class GroupsRepository : IRepository<Group>
     {
+        CoursesContext context;
+
         public void Create(Group t)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Group t)
-        {
-            throw new NotImplementedException();
+            context.Groups.Add(t);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var group = context.Groups.Find(id);
+            context.Groups.Remove(group);
         }
 
         public IEnumerable<Group> Find(Func<Group, bool> predicate)
         {
-            throw new NotImplementedException();
+            return context
+                .Groups
+                .Include(g => g.Students)
+                .Where(predicate)
+                .ToList();
         }
 
-        public Group Get(Group t)
+        public Group Get(int id)
         {
-            throw new NotImplementedException();
+            return context.Groups.Find(id);
         }
 
         public IEnumerable<Group> GetAll()
         {
-            throw new NotImplementedException();
+            return context
+                .Groups
+                .Include(g => g.Students);
+                
         }
 
         public void Update(Group t)
         {
-            throw new NotImplementedException();
+            context.Entry<Group>(t).State = EntityState.Modified;
         }
     }
 }
