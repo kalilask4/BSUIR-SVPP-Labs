626880c38cc64c4d5db08c0b7adaf19d46ef8b73
kalilask4 <kalilask4@gmail.com>
2021-07-30 20:17:11 +0300

    add context

---
+++ WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/EFContext/CoursesContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_lab10.DataLayer.Entities;
+
+namespace WPF_lab10.DataLayer.EFContext
+{
+    
+class CoursesContext : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Group> groups { get; set; }
+        public CoursesContext()
+        {
+            Database.SetInitializer(new CoursesInitializer());
+        }
+
+
+    }
+}
