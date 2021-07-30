ec3193379514fb67442aefdb745de271bc9bf83c
kalilask4 <kalilask4@gmail.com>
2021-07-30 20:17:46 +0300

    add interface IRepository

---
+++ WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_lab10.DataLayer.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Create(T t);
+        void Update(T t);
+        void Delete(T t);
+        T Get(T t);
+        void Delete(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Func<T, bool> predicate);
+    }
+}
