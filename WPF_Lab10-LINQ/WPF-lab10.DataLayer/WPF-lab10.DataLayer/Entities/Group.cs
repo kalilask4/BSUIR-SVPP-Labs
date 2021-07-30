a8bc70206ddaf7ae42d499aec58819008f3d0352
kalilask4 <kalilask4@gmail.com>
2021-07-30 19:57:30 +0300

    add models

---
+++ WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/Entities/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_lab10.DataLayer.EFContext
+{
+    class Group
+    {
+        public int GroupId { get; set; }
+        public string CourseName { get; set; }
+        public DateTime Commence { get; set; }
+        public decimal BasePrice { get; set; }
+        public List<Student> students { get; set; }
+
+        public Group()
+        {
+            students = new List<Student>();
+        }
+    }
+}
---
+++ WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_lab10.DataLayer.EFContext
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal IndividualPrice { get; set; }
+        public string FileName { get; set; }
+        public int GroupId { get; set; }
+        public Group group { get; set; }
+    }
+}
