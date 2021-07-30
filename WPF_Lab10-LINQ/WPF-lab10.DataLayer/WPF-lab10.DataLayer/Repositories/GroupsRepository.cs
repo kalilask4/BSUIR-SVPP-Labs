5fe9f58f428f482a6a460235267ea314f7f7b455
kalilask4 <kalilask4@gmail.com>
2021-07-30 20:49:55 +0300

    add GroupsRepository, implement

---
+++ WPF_Lab10-LINQ/WPF-lab10.DataLayer/WPF-lab10.DataLayer/Repositories/GroupsRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_lab10.DataLayer.Entities;
+using WPF_lab10.DataLayer.Interfaces;
+
+namespace WPF_lab10.DataLayer.Repositories
+{
+    class GroupsRepository : IRepository<Group>
+    {
+        public void Create(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> Find(Func<Group, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group Get(Group t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Group> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Group t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
