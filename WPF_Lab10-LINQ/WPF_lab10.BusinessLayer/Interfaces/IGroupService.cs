1b330563c845864159103bc0b957380315208fd8
kalilask4 <kalilask4@gmail.com>
2021-08-02 01:06:57 +0300

    add interfase IGroupService

---
+++ WPF_Lab10-LINQ/WPF_lab10.BusinessLayer/Interfaces/IGroupService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_lab10.BusinessLayer.Model;
+
+namespace WPF_lab10.BusinessLayer.Interfaces
+{
+    public interface IGroupService
+    {
+        ObservableCollection<GroupViewModel> GetAll();
+        GroupViewModel Get(int id);
+        void AddStudentToGroup(int groupId, StudentViewModel student);
+        void RemoveStudentFromGroup(int groupId, int studentId);
+        void CreateGroup(GroupViewModel group);
+        void DeleteGroup(int groupId);
+        void UpdateGroup(GroupViewModel group);
+    }
+}
